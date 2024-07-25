9d689d6c40fa2c122773676e4fb249773a3daa99
kris_borre <kris_borre@DESKTOP-V0LQE95>
2024-07-25 14:19:04 +0200

    Kinematics in one dimension

---
+++ ConsoleKinematics24may2024/Program.cs
@@ -0,0 +1,40 @@
+﻿using LibraryPhysicalUnits8may2024;
+
+namespace ConsoleKinematics24may2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Schaum's Physics");
+            Console.WriteLine("Chapter3: Kinematics in one dimension");
+            Console.WriteLine("page 46");
+            Console.WriteLine("Exercise 3.50");
+
+            Console.WriteLine("Just as a car starts to accelerate from rest with acceleration 1.4 m/s^2, a bus moving with constant speed of 12 m/s passes it in a parallel lane.");
+            Console.WriteLine("(a) How long before the car overtakes the bus?");
+            Console.WriteLine("(b) How fast will the car then be going?");
+            Console.WriteLine("(c) How far will the car then have gone?");
+
+            var acceleration_car = new AccelerationInMeterPerSecondSquared17may2024(1.4);
+            var speed_bus = new VelocityInMeterPerSecond13may2024(12);
+
+            Console.WriteLine("(a)");
+
+            var time = speed_bus / (acceleration_car / 2);
+            Console.WriteLine("time = " + time); // 17.14 s
+
+            Console.WriteLine("(b)");
+
+            var speed_car = acceleration_car * time;
+            Console.WriteLine("speed_car = " + speed_car); // 24 m/s
+
+            Console.WriteLine("(c)");
+
+            var distance_car = acceleration_car * 0.5 * time.Square();
+            Console.WriteLine("distance_car = " + distance_car); // 205 m
+
+            Console.ReadKey();
+        }
+    }
+}
--- LibraryPhysicalUnits8may2024.Test/UnitTestVelocity9may2024.cs
+++ LibraryPhysicalUnits8may2024.Test/UnitTestVelocity9may2024.cs
@@ -204,5 +204,40 @@ namespace LibraryPhysicalUnits8may2024.Test
             Console.WriteLine("v = " + strike_velocity + " (downward motion)"); // v = -61,30415972835775 m/s (downward motion)
             Assert.Equal(expected: -61.3, actual: strike_velocity.GetInMeterPerSecond(), tolerance: 0.01);
         }
+
+        [Fact]
+        public void Test11()
+        {
+            Console.WriteLine("Schaum's Physics");
+            Console.WriteLine("Chapter3: Kinematics in one dimension");
+            Console.WriteLine("page 46");
+            Console.WriteLine("Exercise 3.50");
+
+            Console.WriteLine("Just as a car starts to accelerate from rest with acceleration 1.4 m/s^2, a bus moving with constant speed of 12 m/s passes it in a parallel lane.");
+            Console.WriteLine("(a) How long before the car overtakes the bus?");
+            Console.WriteLine("(b) How fast will the car then be going?");
+            Console.WriteLine("(c) How far will the car then have gone?");
+
+            var acceleration_car = new AccelerationInMeterPerSecondSquared17may2024(1.4);
+            var speed_bus = new VelocityInMeterPerSecond13may2024(12);
+
+            Console.WriteLine("(a)");
+
+            var time = speed_bus / (acceleration_car / 2);
+            Console.WriteLine("time = " + time); // 17.14 s
+            Assert.Equal(expected: 17, actual: time.GetInSeconds(), tolerance: 1);
+
+            Console.WriteLine("(b)");
+
+            var speed_car = acceleration_car * time;
+            Console.WriteLine("speed_car = " + speed_car); // 24 m/s
+            Assert.Equal(expected: 24, actual: speed_car.GetInMeterPerSecond(), tolerance: 1);
+
+            Console.WriteLine("(c)");
+
+            var distance_car = acceleration_car * 0.5 * time.Square();
+            Console.WriteLine("distance_car = " + distance_car); // 205 m
+            Assert.Equal(expected: 204, distance_car.GetInMeter(), tolerance: 10);
+        }
     }
 }
